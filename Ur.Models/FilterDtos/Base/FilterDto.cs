3939816b103e26967b6657208aa508e35dd4884b
Kristiyan.Karamilev <karamilevkristiyan@gmail.com>
2024-11-21 09:16:00 +0200

    Server: Repositories initialization

---
+++ Ur.Models/FilterDtos/Base/FilterDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Ur.Models.FilterDtos.Base
+{
+    public abstract class FilterDto<TEntity> : IFilterDto<TEntity>
+        where TEntity : class
+    {
+        public int Limit { get; set; } = 30;
+        public int Offset { get; set; }
+        public bool GetAllData { get; set; } = false;
+
+        public string TextFilter { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public virtual IQueryable<TEntity> WhereBuilder(IQueryable<TEntity> query)
+        {
+            return query;
+        }
+    }
+}
---
+++ Ur.Models/FilterDtos/Base/IFilterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Ur.Models.FilterDtos.Base
+{
+    public interface IFilterDto<TEntity>
+    {
+        int Limit { get; set; }
+        int Offset { get; set; }
+        bool GetAllData { get; set; }
+        string TextFilter { get; set; }
+
+        IQueryable<TEntity> WhereBuilder(IQueryable<TEntity> query);
+    }
+}
